dc922ea73cb694abea76d438f086f999e71eae59
GUCANGRAN\GuaiWenWo <1067454662@qq.com>
2021-02-10 12:19:01 +0800

    blog

--- upload.bat
+++ upload.bat
@@ -1,7 +1,4 @@
-    git remote -v
-    git remote rm origin    
-    git remote add origin https://github.com/GuCangRan/blog.git
-    git remote -v
+
     git add .               
     git commit -m "blog"     
     git push origin main
